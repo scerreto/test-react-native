3ad0fa5db32e2396f8f8b852b9811e2c27251936
scerreto <scerreto@wellnet.it>
2017-12-13 12:45:30 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Root } from './js/router';
+
+class App extends Component {
+    render() {
+        return <Root />;
+    }
+}
+
+export default App;
+
